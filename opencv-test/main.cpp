6aff1148a6cc07ebc0c55d03fadf0ab2ff63790b
priyanshu-bajpai <priyanshubajpai9@gmail.com>
2015-09-28 16:48:34 +0530

    Initial Commit

---
+++ opencv-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv-test
+//
+//  Created by R K Bajpai on 28/09/15.
+//  Copyright (c) 2015 R K Bajpai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
